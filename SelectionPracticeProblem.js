006e71d925f9875a8d842a42937646f7bc9aa4f8
Prateek <prateekvasanthpai@gmail.com>
2022-07-30 11:14:53 +0530

    Check Day and Month falls between Mar 20 and Jun 20

--- SelectionPracticeProblem.js
+++ SelectionPracticeProblem.js
@@ -52,3 +52,34 @@ if(num5<num2 && num5<num3 && num5<num4 && num5<num1)
 {
     console.log(num5 + " is minimum number");
 }
+
+
+
+//Checking Day falls between a date or not
+console.log("Check day of month is between March 20 and June 20");
+
+let day=(Math.floor(Math.random() * 10)%31)+1;
+let month=(Math.floor(Math.random() * 10)%6)+1;
+
+console.log("Day : "+day+" & Month : "+month);
+if ( day> 20 && day< 31 && month == 3 )
+{
+    console.log("Input date is within range.");
+}
+else if ( day> 1 && day< 30 && month == 4 )
+{
+    console.log("Input date is within range.");
+}
+else if ( day> 1 && day< 31 && month == 5 )
+{
+    console.log("Input date is within range.");
+}
+else if ( day> 1 && day< 20 && month == 6 )
+{
+    console.log("Input date is within range.");
+}
+else
+{
+    console.log("False");
+}
+
