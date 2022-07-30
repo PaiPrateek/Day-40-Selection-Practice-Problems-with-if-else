c55f43b66b93dbce854dd7b9f14287338e9d46a6
Prateek <prateekvasanthpai@gmail.com>
2022-07-30 11:16:13 +0530

    Check Leap Year

--- SelectionPracticeProblem.js
+++ SelectionPracticeProblem.js
@@ -83,3 +83,16 @@ else
     console.log("False");
 }
 
+//Checking Leap Year between 1000 & 9999;
+let year = (Math.floor(Math.random() *9000)+1000);
+console.log("Checking the mentioned year "+year);
+if (year % 400 == 0 || year % 100 == 0 &&  year % 4 == 0 )
+{
+    console.log(year + " is a leap year");
+}
+else
+{
+    console.log(year + " is not a leap year"); 
+}
+
+
