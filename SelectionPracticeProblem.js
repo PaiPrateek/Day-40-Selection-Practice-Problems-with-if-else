76da563ea8cb803c7bd9bd2f3ea1dcff14c4ebdc
Prateek <prateekvasanthpai@gmail.com>
2022-07-30 11:18:11 +0530

    Flip Coin and print Heads or Tails

--- SelectionPracticeProblem.js
+++ SelectionPracticeProblem.js
@@ -96,3 +96,13 @@ else
 }
 
 
+//Print Head and Tail Simultaneously
+let toss = Math.floor(Math.random()*10) % 2;
+if(toss == 1)
+{
+    console.log("Head");
+}
+else
+{
+    console.log("Tail");
+}
